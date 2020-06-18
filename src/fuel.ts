08a45a35e074386f080ac03cecb8e630b70cfd19
Aaron Cox <aaron@greymass.com>
2020-06-18 01:32:59 -0400

    Adding more chains

--- src/fuel.ts
+++ src/fuel.ts
@@ -2,8 +2,12 @@ import {LinkSession} from 'anchor-link'
 import {SigningRequest} from 'eosio-signing-request'
 
 const supportedChains = {
+    'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906':
+        'https://eos.greymass.com',
     '2a02a0053e5a8cf73a56ba0fda11e4d92e0238a4a2aa74fccf46d5a910746840':
         'https://jungle3.greymass.com',
+    '4667b205c6838ef70ff7988f6e8257e8be0e1284a2f59699054a018f743b1d11':
+        'https://telos.greymass.com',
 }
 
 async function apiCall(url: string, body?: any) {
