f7c319d949ba1a09b1b543c7095f41ba789d16ff
Aaron Cox <aaron@greymass.com>
2020-11-14 14:18:01 -0500

    Updated info key to new methods

--- src/index.ts
+++ src/index.ts
@@ -450,9 +450,9 @@ export default class BrowserTransport implements LinkTransport {
                 throw new Error('Fuel API timeout after 3500ms')
             })
             const modified = await Promise.race([result, timeout])
-            const info = modified.getInfo()
-            if (info.fuel_fee) {
-                const accept = await this.showFee(modified, info.fuel_fee)
+            const fee = modified.getInfoKey('fuel_fee', 'string')
+            if (fee) {
+                const accept = await this.showFee(modified, String(fee))
                 if (accept) {
                     return modified
                 }
