c4a00e308abfd562b2389d9c515066d75e865072
Aaron Cox <aaron@greymass.com>
2020-12-08 14:54:56 -0500

    Moved supportedChains to be an optional configuration parameter

--- src/fuel.ts
+++ src/fuel.ts
@@ -1,14 +1,6 @@
 import type {LinkSession} from 'anchor-link'
 import {Signature, SigningRequest} from 'anchor-link'
 
-const supportedChains = {
-    aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906: 'https://eos.greymass.com',
-    '2a02a0053e5a8cf73a56ba0fda11e4d92e0238a4a2aa74fccf46d5a910746840':
-        'https://jungle3.greymass.com',
-    '4667b205c6838ef70ff7988f6e8257e8be0e1284a2f59699054a018f743b1d11':
-        'https://telos.greymass.com',
-}
-
 async function apiCall(url: string, body?: any) {
     return (
         await fetch(url, {
@@ -25,6 +17,7 @@ export async function fuel(
     request: SigningRequest,
     session: LinkSession,
     updatePrepareStatus: (message: string) => void,
+    supportedChains,
     fuelReferrer = 'teamgreymass'
 ) {
     updatePrepareStatus('Detecting if Fuel is required.')
